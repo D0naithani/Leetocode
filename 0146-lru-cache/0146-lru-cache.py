af3a4a05999d7236577061dff8564e17b46361d0
D0naithani <divanaithani7@gmail.com>
2025-06-09 23:09:43 +0530

    Time: 175 ms (20.03%), Space: 78.5 MB (46.26%) - LeetHub

---
+++ 0146-lru-cache/0146-lru-cache.py
@@ -0,0 +1,52 @@
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache:
+
+    def __init__(self, capacity: int):
+        self.capacity = capacity
+        self.cache = {}  # maps key to node
+
+        # Create dummy head and tail nodes
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    # Remove node from list
+    def _remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    # Add node to front
+    def _add_to_front(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key: int) -> int:
+        if key in self.cache:
+            node = self.cache[key]
+            self._remove(node)
+            self._add_to_front(node)
+            return node.val
+        return -1
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.cache:
+            self._remove(self.cache[key])
+        node = Node(key, value)
+        self._add_to_front(node)
+        self.cache[key] = node
+
+        if len(self.cache) > self.capacity:
+            # Remove from tail
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.cache[lru.key]
